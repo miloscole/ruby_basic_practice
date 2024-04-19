34a122997eac0877049df289954d9bbfdd06ab02
miloscole <27miloscole@gmail.com>
2024-04-19 13:00:20 +0200

    Inital commit, add bubble sort

---
+++ sorting_algorithms/bubble_sort/bubble_sort.rb
@@ -0,0 +1,18 @@
+def bubble_sort(array)
+  n = array.length
+
+  (n - 1).downto(0) do |j|
+    swapped = false
+    for i in 0...j
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+
+  array
+end
+
+p bubble_sort [3, 4, 5, 1, 2, 3, 9, 7]
